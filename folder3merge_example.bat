3cb4ee4d1dc73f86e4327d59b73cff40064e1f12
minger0 <mr.adm1@hotmail.com>
2015-01-15 00:08:27 +0100

    Create folder3merge_example.bat

---
+++ folder3merge_example.bat
@@ -0,0 +1,8 @@
+set FOLDER3MERGE_SKIPLISTFILE=.
+set FOLDER3MERGE_OUTPUTFILE=
+set FOLDER3MERGE_BASE=C:/my/checkouts/tag_v1/
+set FOLDER3MERGE_OTHER=C:/my/checkouts/tag_v2/
+set FOLDER3MERGE_YOURS=C:/my/checkouts/my_changes_on_v1/
+set FOLDER3MERGE_OUTPUT=C:/my/checkouts/my_changes_on_v2/
+set FOLDER3MERGE_MERGETOOL=p4merge
+ruby folder3merge.rb
